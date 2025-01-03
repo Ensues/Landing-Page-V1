a6c6dfa62846e26c09986c27caf2ea576c094739
Ensues <janssenquiambao@gmail.com>
2025-01-03 17:18:05 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,57 @@
+window.addEventListener("load", () => {
+    const loader = document.querySelector(".loader");
+
+    loader.classList.add("loader--hidden");
+
+    loader.addEventListener("transitionend", () => {
+        document.body.removeChild(document.querySelector(".loader"));
+    });
+});
+
+// menu icon
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+let sections = document. querySelectorAll('.section');
+let naviinks = document. querySelectorAll('header nava');
+
+window.onscroll = () => {
+    sections. forEach(sec => {
+        let top = window.scrolly;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+        
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a [href*=' + id + ' ]').classList.add('active')
+            })
+        }
+    })
+}
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+// Light mode
+
+document.getElementById('theme-toggle').addEventListener('click', function() {
+    document.body.classList.toggle('light-mode');
+    document.querySelector('.header').classList.toggle('light-mode');
+    document.querySelector('.navbar').classList.toggle('light-mode');
+    document.querySelector('.footer').classList.toggle('light-mode');
+    document.querySelector('.logo').classList.toggle('light-mode'); 
+    document.querySelector('span').classList.toggle('light-mode');
+    document.querySelector('.btn').classList.toggle('light-mode'); 
+    document.querySelector('.btn-group').classList.toggle('light-mode');
+    document.querySelector('.go-to-btn').classList.toggle('light-mode');
+    document.querySelector('.color-btn').classList.toggle('light-mode');
+    document.querySelector('.home').classList.toggle('light-mode');
+    document.querySelector('.social').classList.toggle('light-mode');
+    document.querySelector('.social-icons').classList.toggle('light-mode');
+    document.querySelector('.home-content').classList.toggle('light-mode'); 
+    document.querySelector('.contact').classList.toggle('light-mode');
+    // document.querySelector('.').classList.toggle('light-mode'); <-- Add this line
+});
